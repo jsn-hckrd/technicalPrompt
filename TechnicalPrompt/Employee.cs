54cc1f808f8760d4bda4f40fcf206814b08872d0
Jason Heckard <jason-heckard@ouhsc.edu>
2021-08-19 11:23:33 -0500

    Adding required files for project

---
+++ TechnicalPrompt/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; } // FK to Department.Id
+        public string Position { get; set; }
+        public int? BadgeNumber { get; set; }
+        public DateTime? HiredDate { get; set; }
+        public float ProductivityScore { get; set; }
+    }
+}
---
+++ TechnicalPrompt/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public int AssigneeId { get; set; } // FK to Employee.Id
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
